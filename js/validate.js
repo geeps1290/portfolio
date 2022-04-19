117644c5a8adbedeab60c46e1950b4604423f25d
Georgina Pickworth <95698572+geeps1290@users.noreply.github.com>
2022-04-19 12:14:16 +0100

    updates to form validation js

--- js/validate.js
+++ js/validate.js
@@ -4,74 +4,89 @@
 
 // Get elements from the DOM
 
+const form = document.querySelector('form');
+const thankYou = document.querySelector('.thank-you');
 const email = document.getElementById('eMail');
 const fname = document.getElementById('fname');
 const subject = document.getElementById('subject');
-
-// ======= Required Field Validation ======= //
-
-fname.addEventListener("blur", requiredName);
-email.addEventListener("blur", requiredEmail);
-subject.addEventListener("blur", requiredSubject);
-
-
-function requiredName() {
-    if(fname.value === '' || fname.value == null) {
-        fname.classList.add("required");
-    } else {
-        fname.classList.remove("required");
-    }
-}
-
-function requiredEmail() {
-    if(email.value === '' || email.value == null) {
-        email.classList.add("required");
-    } else {
-        email.classList.remove("required");
-    }
-}
-
-function requiredSubject() {
-    if(subject.value === '' || subject.value == null) {
-        subject.classList.add("required");
-    } else {
-        subject.classList.remove("required");
-    }
-}
-
-// ======= RegEx validation ======= //
-
-// Get elements from the DOM
-
 const phone = document.getElementById('pnumber');
-const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
 
+// Assign phone and email regEx variables
+
+const regEx_Phone = /(?:(\(?(?:0(?:0|11)\)?[\s-]?\(?|\+?)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|\(?0)((?:\d{5}\)?[\.\s-]?\d{4,5})|(?:\d{4}\)?[\.\s-]?(?:\d{3}[\.\s-]?\d{3}))|(?:\d{4}\)?[\.\s-]?(?:\d{5}))|(?:\d{3}\)?[\.\s-]?\d{3}[\.\s-]?\d{3,4})|(?:\d{2}\)?[\.\s-]?\d{4}[\.\s-]?\d{4}))(?:[\s-]?((?:x|ext[\.\s]*|\#)\d{3,4})?)/;
+const regEx_Email = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z{2,5}])/;
+
+// Set boolean to keep track of valid input fields
+
+let isFormValid = false;
+let isValidationOn = false;
+
+// ======= Required Field and Regex Validation ======= //
+
+const validateInputs = () => {
+    if(!isValidationOn) return;
+
+    fname.classList.remove("notValid");
+    fname.nextElementSibling.classList.add("hidden");
+    email.classList.remove("notValid");
+    email.nextElementSibling.classList.add("hidden");
+    subject.classList.remove("notValid");
+    subject.nextElementSibling.classList.add("hidden");
+    phone.classList.remove("notValid");
+    phone.nextElementSibling.classList.add("hidden");
+    isFormValid = true;
+
+    if (!fname.value) {
+        fname.nextElementSibling.classList.remove("hidden")
+        fname.classList.add("notValid")
+        isFormValid = false;
+    }
+    if (!email.value) {
+        email.nextElementSibling.classList.remove("hidden")
+        email.classList.add("notValid")
+        isFormValid = false;
+    }
+    if (!subject.value) {
+        subject.nextElementSibling.classList.remove("hidden")
+        subject.classList.add("notValid")
+        isFormValid = false;
+    }
+    if (!regEx_Phone.test(phone.value) && phone.value != "") {
+        phone.nextElementSibling.classList.remove("hidden")
+        phone.classList.add("notValid")
+        isFormValid = false;
+    }
+    if (!regEx_Email.test(email.value)) {
+        email.nextElementSibling.classList.remove("hidden")
+        email.classList.add("notValid")
+        isFormValid = false;
+    }
+
+};
 
 form.addEventListener('submit', (e) => {
-    let messages = [];
-
-    const regEx_Phone = /(?:(\(?(?:0(?:0|11)\)?[\s-]?\(?|\+?)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|\(?0)((?:\d{5}\)?[\.\s-]?\d{4,5})|(?:\d{4}\)?[\.\s-]?(?:\d{3}[\.\s-]?\d{3}))|(?:\d{4}\)?[\.\s-]?(?:\d{5}))|(?:\d{3}\)?[\.\s-]?\d{3}[\.\s-]?\d{3,4})|(?:\d{2}\)?[\.\s-]?\d{4}[\.\s-]?\d{4}))(?:[\s-]?((?:x|ext[\.\s]*|\#)\d{3,4})?)/;
-    const regEx_Email = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z{2,5}])/;
-
-    var i = !regEx_Phone.test(phone.value)
-    var j = !regEx_Email.test(email.value)
-  
-    if (i) {
-      phone.classList.add("notValid")
+    e.preventDefault();
+    isValidationOn = true;
+    validateInputs();
+    if (isFormValid) {
+        form.remove();
+        thankYou.classList.remove('hidden');
     }
-    
-    if (j) {
-      email.classList.add("notValid")
-    }
+})
 
-    if (i == true || j == true) {
-            messages.push('*Please enter email/phone number in correct format')
-          }
-  
-    if (messages.length > 0) {
-      e.preventDefault()
-      errorElement.innerText = messages.join(', ')
-      errorElement.classList.add("error")
-    }
-  })
+fname.addEventListener('input', () => {
+    validateInputs();
+})
+
+email.addEventListener('input', () => {
+    validateInputs();
+})
+
+subject.addEventListener('input', () => {
+    validateInputs();
+})
+
+phone.addEventListener('input', () => {
+    validateInputs();
+})
